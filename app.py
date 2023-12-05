b58daa75fde0e9f3f1aa58ac3c305a228ea030a4
Dj Sai <87787448+iamdjsai9999@users.noreply.github.com>
2023-12-05 14:17:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,6 @@ def scrape_links(url):
         raise Exception('An error occurred: ' + str(e))
 
 if __name__ == '__main__':
+    # Use the provided PORT environment variable by Render.com
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)  # Set debug to False in production
